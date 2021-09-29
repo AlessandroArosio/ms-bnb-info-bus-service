f4850f7ec411a322695727dd4de7ab6467ec6015
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-09-29 16:31:44 +0100

    Adding base project that points to BOM

---
+++ src/main/java/com/aledev/alba/msbnbinfobusservice/MsBnbInfoBusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aledev.alba.msbnbinfobusservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBnbInfoBusServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBnbInfoBusServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aledev/alba/msbnbinfobusservice/MsBnbInfoBusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aledev.alba.msbnbinfobusservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBnbInfoBusServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
